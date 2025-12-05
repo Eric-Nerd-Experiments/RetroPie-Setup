c344bb1c2a084191b744cfe2f1b511d26bbd526c
Eric-Nerd-Experiments <130358237+Eric-Nerd-Experiments@users.noreply.github.com>
2025-12-05 12:54:33 -0800

    Update lr-smsplus-gx.sh
    add additional system options

--- scriptmodules/libretrocores/lr-smsplus-gx.sh
+++ scriptmodules/libretrocores/lr-smsplus-gx.sh
@@ -36,7 +36,7 @@ function install_lr-smsplus-gx() {
 
 function configure_lr-smsplus-gx() {
     local system
-    for system in gamegear mastersystem; do
+    for system in gamegear gamegearh gamegear-japan gamegear-proto gamegear-unl markiii markiii-t mastersystemh mastersystem-proto mastersystem-unl mastersystem; do
         mkRomDir "$system"
         defaultRAConfig "$system"
         addEmulator 0 "$md_id" "$system" "$md_inst/smsplus_libretro.so"
