4c576d5873fe19050008ae26fb9607e9a36ed2d5
Eric-Nerd-Experiments <130358237+Eric-Nerd-Experiments@users.noreply.github.com>
2025-12-05 10:54:49 -0800

    Update lr-atari800.sh
    cleanup

--- scriptmodules/libretrocores/lr-atari800.sh
+++ scriptmodules/libretrocores/lr-atari800.sh
@@ -34,29 +34,15 @@ function install_lr-atari800() {
 }
 
 function configure_lr-atari800() {
-    mkRomDir "atari800"
-    mkRomDir "atari5200"
-    mkRomDir "atari5200h"
-    mkRomDir "atari5200-proto"
-    mkRomDir "atari5200-unl"
-
-    defaultRAConfig "atari800"
-    defaultRAConfig "atari5200"
-    defaultRAConfig "atari5200h"
-    defaultRAConfig "atari5200-proto"
-    defaultRAConfig "atari5200-unl"
-
+   
     mkUserDir "$md_conf_root/atari800"
     moveConfigFile "$home/.lr-atari800.cfg" "$md_conf_root/atari800/lr-atari800.cfg"
 
-    addEmulator 1 "lr-atari800" "atari800" "$md_inst/atari800_libretro.so"
-    addEmulator 1 "lr-atari800" "atari5200" "$md_inst/atari800_libretro.so"
-    addEmulator 1 "lr-atari800" "atari5200h" "$md_inst/atari800_libretro.so"
-    addEmulator 1 "lr-atari800" "atari5200-proto" "$md_inst/atari800_libretro.so"
-    addEmulator 1 "lr-atari800" "atari5200-unl" "$md_inst/atari800_libretro.so"
-    addSystem "atari800"
-    addSystem "atari5200"
-    addSystem "atari5200h"
-    addSystem "atari5200-proto"
-    addSystem "atari5200-unl"
+local system
+    for system in atari800 atari5200 atari5200h atari5200-proto atari5200-unl; do
+    mkRomDir "$system"
+    defaultRAConfig "$system"
+    addEmulator 1 "$md_id" "$system" "$md_inst/atari800_libretro.so"
+    addSystem "$system"
+    
 }
