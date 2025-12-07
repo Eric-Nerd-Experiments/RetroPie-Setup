421dba5ed5c044a84324b853ae0b44684d25dc7e
Eric-Nerd-Experiments <130358237+Eric-Nerd-Experiments@users.noreply.github.com>
2025-12-06 23:18:00 -0800

    Update lr-stella2014.sh

--- scriptmodules/libretrocores/lr-stella2014.sh
+++ scriptmodules/libretrocores/lr-stella2014.sh
@@ -36,7 +36,7 @@ function install_lr-stella2014() {
 
 function configure_lr-stella2014() {
     local system
-    for system in atari2600 atari2600h atari2600-proto atari2600-unl; do
+    for system in atari2600 atari2600h atari2600-proto atari2600-hb; do
     mkRomDir "$system"
     defaultRAConfig "$system"
     addEmulator 0 "$md_id" "$system" "$md_inst/stella2014_libretro.so"
