b2c3539ba6702d25afcce317ca921818731d7753
Eric-Nerd-Experiments <130358237+Eric-Nerd-Experiments@users.noreply.github.com>
2025-12-07 12:03:19 -0800

    Update lr-np2kai.sh
    add translated

--- scriptmodules/libretrocores/lr-np2kai.sh
+++ scriptmodules/libretrocores/lr-np2kai.sh
@@ -36,7 +36,11 @@ function install_lr-np2kai() {
 function configure_lr-np2kai() {
     mkRomDir "pc98"
     defaultRAConfig "pc98"
+    mkRomDir "pc98-t"
+    defaultRAConfig "pc98-t"
 
     addEmulator 1 "$md_id" "pc98" "$md_inst/np2kai_libretro.so"
     addSystem "pc98"
+    addEmulator 1 "$md_id" "pc98-t" "$md_inst/np2kai_libretro.so"
+    addSystem "pc98-t"
 }
